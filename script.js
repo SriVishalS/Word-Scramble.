50403a2494eab9d08fbf7d937fab4cafa0d7852b
sriVishalS <vishalS221202@gmail.com>
2022-10-29 17:16:06 +0530

    A Word Scramble Game

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const initTimer = (maxTime) => {
 };
 
 const initGame = () => {
-  initTimer(30);
+  initTimer(60);
   let randomObj = words[Math.floor(Math.random() * words.length)];
   let wordArray = randomObj.word.split("");
   for (let i = wordArray.length - 1; i > 0; i--) {
